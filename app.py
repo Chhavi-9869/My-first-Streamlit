51ca63219679e06a83aa4b6148dd500c025c7c48
Chhavi-9869 <chhavibothra24@gmail.com>
2025-08-06 11:36:19 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.title("My first Streamlit app")
+st.write("This is the place where you can write any description or any text")
+#For slider
+slider_value=st.slider("Select a value", 0, 100, 50)
+st.write(f"You selected: {slider_value}")
+#checkbox
+st.title("Interactive Checbox Example")
+checkbox_value=st.checkbox("Check me")
+if checkbox_value:
+    st.write("Checkbox is checked!")
+else:
+    st.write("Checkbox is not checked!")
+    
+#For text
+st.title("Text Input Example")
+text_value = st.text_input("Enter some text")       
+st.write(f"You entered: {text_value}")
